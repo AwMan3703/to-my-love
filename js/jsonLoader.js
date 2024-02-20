dc8ee762ee8938b0e29efc78dd1b7b0b5c161aaa
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-02-20 18:27:09 +0100

    that's better

--- js/jsonLoader.js
+++ js/jsonLoader.js
@@ -2,8 +2,7 @@
 // totally-not-stackoverflow-copy-paste code that was totally written by me
 // https://stackoverflow.com/a/70780779/14647521 (in case I need it again)
 async function getJSON(url) {
-    let res = await fetch(url).then(response => response.json())
-    console.log(res)
+    let r = await fetch(url).then(response => response.json())
+    console.log("Fetched JSON data from " + url+ " => " + r)
+    return r
 }
-
-console.log(getJSON("data/myjson.json"))
