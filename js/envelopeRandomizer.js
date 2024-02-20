e5c8b980f14c8cd8927f18b3dca5091a858b03ca
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-02-20 22:47:17 +0100

    desperately trying to make this work

--- js/envelopeRandomizer.js
+++ js/envelopeRandomizer.js
@@ -1,11 +1,39 @@
 
-const envelope = document.getElementById("envelope")
-const flap = envelope.getElementById("flap")
-const body = envelope.getElementById("body")
-const inside = envelope.getElementById("inside")
+const prefix = "randomEnvelope-"
+const flap = document.getElementById(prefix + "flap")
+const body = document.getElementById(prefix + "body")
+const inside = document.getElementById(prefix + "inside")
+const stamps = document.getElementById(prefix + "stamps")
+
+let randomizationData;
+fetch('../data/envelopeRandomization.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+        return response.json();
+    })
+    .then(data => {
+        randomizationData = data;
+        console.log(randomizationData);
+    })
+    .catch(error => {
+        console.error('Error fetching randomization data:', error);
+    });
+console.log(randomizationData);
+
 
 const randomize = () => {
+    const envelopePath = "assets/envelope"
+    const stampsPath = `assets/stamps/`
+    const envelopeN = randInt(1, 1)
+    const stampsN = randInt(1, 1)
+    inside.src = envelopePath + envelopeN + "/inside.png"
+    body.src = envelopePath + envelopeN + "/body.png"
+    flap.src = envelopePath + envelopeN + "/flap.png"
+    stamps.src = stampsPath + stampsN + ".png"
 
+    console.log("envelope randomization complete")
 }
 
 randomize()
