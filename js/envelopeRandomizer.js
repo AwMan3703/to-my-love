101196dd725502195a95a20e518a220ea49e0e07
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-02-20 18:27:48 +0100

    created a basic envelope randomizer (it's empty but just you wait)

---
+++ js/envelopeRandomizer.js
@@ -0,0 +1,11 @@
+
+const envelope = document.getElementById("envelope")
+const flap = envelope.getElementById("flap")
+const body = envelope.getElementById("body")
+const inside = envelope.getElementById("inside")
+
+const randomize = () => {
+
+}
+
+randomize()
