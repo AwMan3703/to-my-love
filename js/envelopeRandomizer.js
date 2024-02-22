616fa214d5ca1fd1f6c0da793434b30e0ff5d5c0
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-02-22 12:04:58 +0100

    tidied up some code

--- js/envelopeRandomizer.js
+++ js/envelopeRandomizer.js
@@ -9,22 +9,27 @@ const stamps = document.getElementById(prefix + "stamps")
 
 const animation_phase1 = () => {
     envelope.style.animationName = "letter-reveal-1"
+
     envelope.addEventListener("click", animation_phase2)
 }
 const animation_phase2 = () => {
     envelope.style.animationDuration = ".25s"
     envelope.style.animationTimingFunction = "ease-in"
     envelope.style.animationName = "letter-reveal-2"
+
     envelope.addEventListener("animationend", animation_phase3)
 }
 const animation_phase3 = () => {
     front.style.visibility = "hidden"
     stamps.style.visibility = "hidden"
+
     inside.style.visibility = "visible"
     body.style.visibility = "visible"
     flap.style.visibility = "visible"
+
     envelope.style.animationTimingFunction = "ease-out"
     envelope.style.animationName = "letter-reveal-3"
+
     envelope.removeEventListener("load", animation_phase1)
     envelope.removeEventListener("click", animation_phase2)
     envelope.removeEventListener("animationend", animation_phase3)
@@ -35,9 +40,11 @@ const randomize = (data) => {
     const stampsPath = `assets/stamps/`
     const envelopeN = randInt(1, data.ranges.envelopes)
     const stampsN = randInt(2, data.ranges.stamps) // start from 2 because stamps/1.png is reserved to detect failed randomization
+
     inside.src = envelopePath + envelopeN + "/inside.png"
     body.src = envelopePath + envelopeN + "/body.png"
     flap.src = envelopePath + envelopeN + "/flap.png"
+
     inside.style.visibility = "hidden"
     body.style.visibility = "hidden"
     flap.style.visibility = "hidden"
