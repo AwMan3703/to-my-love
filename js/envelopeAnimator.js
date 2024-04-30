d34406f02043f7a3adb5b23f901fea547c1e3361
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-04-30 12:26:56 +0200

    fixed letter overflowing on mobile

--- js/envelopeAnimator.js
+++ js/envelopeAnimator.js
@@ -31,7 +31,7 @@ const animation_phase4 = () => {
     flap.style.filter = "contrast(0.5)"
 
     letter.style.height = "200%"
-    letter.style.translate = "0 -40%"
+    letter.style.translate = "0 -90%"
 
     envelope.style.translate = "0 50%"
 
