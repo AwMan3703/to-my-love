cc13a9ad8f8f10bdac935ba8d8774b6d4b59426b
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-07-28 22:55:59 +0200

    removed heavy file

--- train-ride/js/tooltip.js
+++ train-ride/js/tooltip.js
@@ -3,7 +3,7 @@ const tooltipElement = document.getElementById('tooltip');
 
 document.querySelectorAll(".scene img.selectable").forEach((e) => {
     e.addEventListener('mouseenter', (_) => {
-        tooltipElement.innerText = e.dataset.tooltipText
+        tooltipElement.innerHTML = e.dataset.tooltipText
     })
 })
 
